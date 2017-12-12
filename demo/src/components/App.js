c3debb96322646959ca8e32af29ad68675a50432
Melody <Melody@Melodys-MacBook.local>
2017-12-12 12:49:30 -0700

    added switch to people

--- demo/src/components/App.js
+++ demo/src/components/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, Link } from 'react-router-dom';
+import { Route, Link, Switch } from 'react-router-dom';
 import Home from './Home/Home';
 import About from './About/About';
 import People from './People/People';
@@ -24,8 +24,10 @@ class App extends Component {
             <Route path="/about/company" component={ Company } />
           </About>
         } />
-        <Route path="/people" component={ People }/>
-        <Route path="/people/:id" component={ Character }/>
+        <Switch>
+          <Route exact path="/people" component={ People }/>
+          <Route path="/people/:id" component={ Character }/>
+        </Switch>
       </div>
     );
   }
