54a0a5ba0cee92e3941f8686a5d2fd28cf6ef5b6
Melody <Melody@Melodys-MacBook.local>
2017-12-12 11:23:43 -0700

    add links

--- demo/src/components/App.js
+++ demo/src/components/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Link } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <div>
         This is App.js
+        <div>
+          <Link to="/" >Home</Link>
+          <Link to="/about" >About</Link>
+          <Link to="/people" >People</Link>
+        </div>
         <Route exact path="/" render={() => <div>Home</div>} />
         <Route path="/about" render={() => <div>About</div>}/>
         <Route path="/people" render={() => <div>People</div>}/>
